a33f90dae9fbcc6ee1eb003196a8ccdc0256ebcf
Bishop <58209884+y0un9er@users.noreply.github.com>
2020-12-09 14:49:23 +0800

    Create run.py
    run.py

---
+++ run.py
@@ -0,0 +1,48 @@
+import AllChapter
+import AllFree
+import Download
+import threading
+
+'''
+    选择下载所有免费漫画或部分
+'''
+
+
+def main():
+    check = input('下载全部免费漫画 或 下载部分 （1 or 2）')
+
+    if check == '1':
+        F = AllFree.FreeUrl()
+        all_comic_url = F.AllUrls()
+
+    else:
+        check2 = input('请选择下载某页，或某范围（1 or 2）')
+        if check2 == '1':
+            url = input('请输入该页的链接')
+            F = AllFree.FreeUrl(url=url)
+            all_comic_url = F.OnePageUrls()
+        else:
+            start = int(input('请输入开始页码（1-116？）'))
+            end = int(input('请输入结束页码（1-116？）'))
+            F = AllFree.FreeUrl(start=start, end=end)
+            all_comic_url = F.AllUrls()
+
+    D = Download.Download()
+    for comic_url in all_comic_url:
+        D.comic_info(comic_url)
+
+        all_info = AllChapter.ChaptersInfo(comic_url)
+
+        ts = []
+
+        for chapter_info in all_info:
+            # D.getImg(chapter_info)
+            ts.append(threading.Thread(target=D.getImg, args=(chapter_info,)))
+
+        for t in ts:
+            t.start()
+            t.join()
+
+
+if __name__ == '__main__':
+    main()
