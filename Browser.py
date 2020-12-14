0eec0b0a87b28b956c2ee7ab12441fb901209cf6
Bishop <58209884+y0un9er@users.noreply.github.com>
2020-12-14 21:01:22 +0800

    Update Browser.py

--- Browser.py
+++ Browser.py
@@ -3,18 +3,32 @@ from selenium import webdriver
 
 '''
     创建 Browser 类是选择 显示浏览器窗口 或 不显示
-    browser2 = Browser('Window').browser
-    browser1 = Browser('Nowindow').browser
+    browser2 = Browser('firefox', 'Window').browser
+    browser1 = Browser('chrome', 'Nowindow').browser
 '''
 
 
 class Browser:
     browser = ''
 
-    def __init__(self, mode='Nowindow'):
+    def __init__(self, browser, mode='Nowindow'):
+        if browser == 'chrome':
+            self.chrome(mode)
+        elif browser == 'firefox':
+            self.firefox(mode)
+
+    def chrome(self, mode):
         if mode == 'Nowindow':
             opt = webdriver.ChromeOptions()
             opt.headless = True
             self.browser = webdriver.Chrome(options=opt)
         elif mode == 'Window':
             self.browser = webdriver.Chrome()
+
+    def firefox(self, mode):
+        if mode == 'Nowindow':
+            opt = webdriver.FirefoxOptions()
+            opt.headless = True
+            self.browser = webdriver.Firefox(options=opt)
+        elif mode == 'Window':
+            self.browser = webdriver.Firefox()
